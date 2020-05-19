9401aebd985cbea04dfaa044f10cbb37ce19fb27
liu <lbw1657792697@163.com>
2020-05-19 20:00:31 +0800

    上传项目

--- src/pages/nav/recommend/Recommend.js
+++ src/pages/nav/recommend/Recommend.js
@@ -4,8 +4,16 @@ export default class Recommend extends Component {
     render() {
         return (
             <div>
-                推荐
+                <div id="container" style={{width:650,height:255}}>a</div>
             </div>
         )
+    }
+    
+    componentDidMount(){
+        var map = new window.AMap.Map("container", {
+            resizeEnable: true, //地图是否可以重置大小
+            center: [116.397428, 39.90923], //以哪个经纬度为中心点开始绘制
+            zoom: 13    //地图默认缩放比
+        });
     }
 }
